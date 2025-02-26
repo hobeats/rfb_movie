6c1fcded92232d8c9465b850bc6fce76f6ccd017
jeho <hobeats@gmail.com>
2025-02-26 09:24:51 +0900

    무비

--- src/App.js
+++ src/App.js
@@ -1,43 +1,35 @@
 import { useEffect, useState } from "react";
+import Movie from "./Movie";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [result, setResult] = useState(0);
-  const [selCoin, setSelCoin] = useState(0);
-  const selected = (e) => {
-    setSelCoin(e.target.value);
-  };
-  const inputMoney = (e) => {
-    setResult(e.target.value);
+  const [laoding, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=rating"
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
   return (
     <div>
-      <h1>THE COINS!</h1>
-      {loading ? (
-        <strong>Loading...</strong>
+      {laoding ? (
+        <h1>Loading...</h1>
       ) : (
         <div>
-          <select onChange={selected}>
-            <option key="-1">코인을 선택하세요</option>
-            {coins.map((coin, index) => (
-              <option key={index} value={coin.quotes.USD.price}>
-                {coin.name}({coin.symbol}): ${coin.quotes.USD.price.toFixed(2)}
-              </option>
-            ))}
-          </select>
-          <div>
-          <input onChange={inputMoney} type="number" placeholder="USD" /> {'> '}
-          <input value={result/selCoin > 0 ? result/selCoin : 0} disabled /> 
-          </div>
+          {movies.map((movie) => (
+            <Movie
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
         </div>
       )}
     </div>
